dad798ae4866f1d96d14e1e44c5155b27f3c3191
Xiro <xiro@duck.com>
2023-12-19 13:23:25 +0100

    feat: Logger middleware

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
-use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use actix_web::{get, middleware::Logger, App, HttpResponse, HttpServer, Responder};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().service(hello))
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    HttpServer::new(|| App::new().service(hello).wrap(Logger::default()))
         .bind(("127.0.0.1", 8080))?
         .run()
         .await
